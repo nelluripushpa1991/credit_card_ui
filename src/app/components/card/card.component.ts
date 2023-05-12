87f8f4490927b80d08fd2b63a91b77ee0ad7ae49
pushpa <nelluripushpa1991@gmail.com>
2023-05-12 11:04:08 -0600

    removed console logs

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -120,7 +120,6 @@ export class CardComponent implements OnInit {
 
   // logic to display appropriate message based error response
   handleError(errorResponse: any) {
-    console.log("errorresponse "+JSON.stringify(errorResponse));
     if (errorResponse !== null && errorResponse !== "") {
       this.messages = [{ severity: 'error', summary: 'Error', detail: errorResponse.statusText }];
     }
